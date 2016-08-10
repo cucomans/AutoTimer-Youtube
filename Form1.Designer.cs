7e849660027de95c8a325790357dc2173be79640
cucomans <mtanevitch@hotmail.com>
2016-08-10 12:30:18 -0300

    Version 1.0.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace timer
+{
+    partial class AutoTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoTime));
+            this.Log = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuBar = new System.Windows.Forms.MenuItem();
+            this.clearItem = new System.Windows.Forms.MenuItem();
+            this.exitItem = new System.Windows.Forms.MenuItem();
+            this.SuspendLayout();
+            // 
+            // Log
+            // 
+            resources.ApplyResources(this.Log, "Log");
+            this.Log.Name = "Log";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuBar});
+            // 
+            // menuBar
+            // 
+            this.menuBar.Index = 0;
+            this.menuBar.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.clearItem,
+            this.exitItem});
+            resources.ApplyResources(this.menuBar, "menuBar");
+            this.menuBar.Click += new System.EventHandler(this.menuItem1_Click);
+            // 
+            // clearItem
+            // 
+            this.clearItem.Index = 0;
+            resources.ApplyResources(this.clearItem, "clearItem");
+            this.clearItem.Click += new System.EventHandler(this.clearItem_Click);
+            // 
+            // exitItem
+            // 
+            this.exitItem.Index = 1;
+            resources.ApplyResources(this.exitItem, "exitItem");
+            this.exitItem.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // AutoTime
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Log);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Menu = this.mainMenu1;
+            this.Name = "AutoTime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuBar;
+        private System.Windows.Forms.MenuItem clearItem;
+        private System.Windows.Forms.MenuItem exitItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using System.Threading;
+using System.Windows.Media;
+
+namespace timer
+{
+   
+
+    public partial class AutoTime : Form
+    {
+       TimeSpan lastDuration = new TimeSpan (0,0,0);
+ 
+            public AutoTime()
+        {
+            InitializeComponent();
+            
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+
+        void Form1_DragEnter(object sender,DragEventArgs e) {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.All;
+        }
+        public static string ExtractFilename(string filepath)
+        {
+            // If path ends with a "\", it's a path only so return String.Empty.
+            if (filepath.Trim().EndsWith(@"\"))
+                return String.Empty;
+
+            // Determine where last backslash is.
+            int position = filepath.LastIndexOf('\\');
+            // If there is no backslash, assume that this is a filename.
+            if (position == -1)
+            {
+               
+                    return filepath.Substring(0,filepath.Length-3);
+
+            }
+            else
+            {
+                // Return filename without file path.
+               filepath =
+                    filepath.Substring(position + 1);
+                filepath= filepath.Substring(0,filepath.Length-3);
+                return filepath;
+
+            }
+        }
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            TimeSpan time = new TimeSpan();
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string mediaFile in files)
+            {
+                var duration = (GetMediaDuration(mediaFile, time));
+                duration = new TimeSpan(duration.Hours, duration.Minutes, duration.Seconds);
+               // duration = duration.Substring(3, 5);
+                 Log.AppendText(ExtractFilename(mediaFile) +" - " +lastDuration + Environment.NewLine);
+                lastDuration = lastDuration.Add(duration);
+            }
+           
+
+        }
+        static TimeSpan GetMediaDuration(string mediaFile)
+        {
+            return GetMediaDuration(mediaFile, TimeSpan.Zero);
+        }
+
+        static TimeSpan GetMediaDuration(string mediaFile, TimeSpan maxTimeToWait)
+        {
+            var mediaData = new MediaData() { MediaUri = new Uri(mediaFile) };
+
+            var thread = new Thread(GetMediaDurationThreadStart);
+            DateTime deadline = DateTime.Now.Add(maxTimeToWait);
+            thread.Start(mediaData);
+
+            while (!mediaData.Done && ((TimeSpan.Zero == maxTimeToWait) || (DateTime.Now < deadline)))
+                Thread.Sleep(100);
+
+            Dispatcher.FromThread(thread).InvokeShutdown();
+
+            if (!mediaData.Done)
+                throw new Exception(string.Format("GetMediaDuration timed out after {0}", maxTimeToWait));
+            if (mediaData.Failure)
+                throw new Exception(string.Format("MediaFailed {0}", mediaFile));
+
+            return mediaData.Duration;
+        }
+
+        static void GetMediaDurationThreadStart(object context)
+        {
+            var mediaData = (MediaData)context;
+            var mediaPlayer = new MediaPlayer();
+
+            mediaPlayer.MediaOpened +=
+                delegate
+                {
+                    if (mediaPlayer.NaturalDuration.HasTimeSpan)
+                        mediaData.Duration = mediaPlayer.NaturalDuration.TimeSpan;
+                    mediaData.Success = true;
+                    mediaPlayer.Close();
+                };
+
+            mediaPlayer.MediaFailed +=
+                delegate
+                {
+                    mediaData.Failure = true;
+                    mediaPlayer.Close();
+                };
+
+            mediaPlayer.Open(mediaData.MediaUri);
+
+            Dispatcher.Run();
+        }
+
+        private void menuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clearItem_Click(object sender, EventArgs e)
+        {
+            Log.Clear();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    class MediaData
+    {
+        public Uri MediaUri;
+        public TimeSpan Duration = TimeSpan.Zero;
+        public bool Success;
+        public bool Failure;
+        public bool Done { get { return (Success || Failure); } }
+    }
+}
+
